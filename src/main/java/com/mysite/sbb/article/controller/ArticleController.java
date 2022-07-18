bb7915305b149a1c2299dfdbb1fec7f518c50778
leehaneul623 <dyj03216@naver.com>
2022-07-18 13:40:37 +0900

    시험종료

--- src/main/java/com/mysite/sbb/article/controller/ArticleController.java
+++ src/main/java/com/mysite/sbb/article/controller/ArticleController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,6 +27,14 @@ public class ArticleController {
         model.addAttribute("articleList",articleList);
 
         return "Article_list";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showArticles(Model model, @PathVariable("id") Integer id){
+        Article article = this.articleService.getArticle(id);
+        model.addAttribute("article",article);
+
+        return "article_detail";
     }
 
 }
--- src/main/java/com/mysite/sbb/article/service/ArticleService.java
+++ src/main/java/com/mysite/sbb/article/service/ArticleService.java
@@ -2,10 +2,12 @@ package com.mysite.sbb.article.service;
 
 import com.mysite.sbb.article.dao.ArticleRepository;
 import com.mysite.sbb.article.domain.Article;
+import com.mysite.sbb.util.DataNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -15,5 +17,14 @@ public class ArticleService {
 
     public List<Article> getList() {
         return articleRepository.findAll();
+    }
+
+    public Article getArticle(Integer id){
+        Optional<Article> article = this.articleRepository.findById(id);
+        if(article.isPresent()){
+            return article.get();
+        } else {
+            throw new DataNotFoundException("article not found");
+        }
     }
 }
